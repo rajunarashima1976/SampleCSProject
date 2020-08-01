b9cea82ee2cfed071a6247400144640dc4513950
User6 <User6@CBAPOOL-0>
2020-08-01 08:47:39 +0000

    Comitting All the Changes

---
+++ SampleAssocaiteCode/DemandService/IDemandService.cs
@@ -0,0 +1,14 @@
+﻿using SampleAssocaiteCode.EntityModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SampleAssocaiteCode.DemandService
+{
+    public interface IDemandService
+    {
+        Task<IQueryable<Demand>> Get();
+        Task<bool> Post(Demand demand);
+    }
+}
---
+++ SampleAssocaiteCode/TalmateDBContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SampleAssocaiteCode.EntityModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SampleAssocaiteCode
+{
+    public class TalmateDBContext:DbContext
+    {
+        public TalmateDBContext(DbContextOptions options) : base(options)
+        {
+        }
+
+
+        public DbSet<Demand> Demands { get; set; }
+    }
+}
