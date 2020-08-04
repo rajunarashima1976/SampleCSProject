495eb8621ef0dbd5744566814da39005a8e9e8a1
User6 <User6@CBAPOOL-0>
2020-08-04 20:02:43 +0000

    Added all the code in controller

---
+++ SampleCSWebProject/Controllers/DashboardController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SampleCSWebProject.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            return await Task.FromResult(View());
+        }
+    }
+}
+
--- SampleCSWebProject/Startup.cs
+++ SampleCSWebProject/Startup.cs
@@ -2,11 +2,15 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using SampleAssocaiteCode.Common;
+using SampleCSWebProject.Models;
 
 namespace SampleCSWebProject
 {
@@ -22,7 +26,15 @@ namespace SampleCSWebProject
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddAutoMapper(typeof(Startup));
+            services.AddControllersWithViews().AddRazorRuntimeCompilation();
+            services.Configure<AppSettings>(Configuration.GetSection("AppSettings"));
+            services.AddSession(options => {
+                options.IdleTimeout = TimeSpan.FromMinutes(10);
+            });
+            services.AddMvc();
+            services.AddHttpContextAccessor();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,9 +47,12 @@ namespace SampleCSWebProject
             else
             {
                 app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
             }
+            app.UseHttpsRedirection();
             app.UseStaticFiles();
-
+            app.UseSession();
             app.UseRouting();
 
             app.UseAuthorization();
@@ -46,7 +61,7 @@ namespace SampleCSWebProject
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Login}/{action=Index}/{id?}");
             });
         }
     }
