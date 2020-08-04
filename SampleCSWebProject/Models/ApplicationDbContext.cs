4216c954e2401a582754b5322ee8f5a6d1543215
User6 <User6@CBAPOOL-0>
2020-08-04 19:16:13 +0000

    Added All Models and ModelDTO classes

---
+++ SampleCSWebProject/Models/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SampleAssocaiteCode.EntityModel;
+using System;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SampleCSWebProject.Models
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        DbSet<Demand> demandrequirement { get; set; }
+    }
+}
