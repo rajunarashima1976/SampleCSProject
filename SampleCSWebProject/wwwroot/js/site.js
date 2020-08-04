6b7e6918afb022812058ab36ad93e1bcd88a1a95
User6 <User6@CBAPOOL-0>
2020-08-04 19:43:38 +0000

    Added all the required js and css and other files

--- SampleCSWebProject/wwwroot/js/site.js
+++ SampleCSWebProject/wwwroot/js/site.js
@@ -1,4 +1,11 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+﻿function isUrlValid(url) {
+    var decodedurl = decodeURI(url);
+    var encodedurl = encodeURI(decodedurl);
+    var pattern = new RegExp('^(https?:\\/\\/)?' + // protocol
+        '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.?)+[a-z]{2,}|' + // domain name
+        '((\\d{1,3}\\.){3}\\d{1,3}))' + // ip (v4) address
+        '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + //port
+        '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
+        '(\\#[-a-z\\d_]*)?$', 'i');
+    return pattern.test(encodedurl);
+}
