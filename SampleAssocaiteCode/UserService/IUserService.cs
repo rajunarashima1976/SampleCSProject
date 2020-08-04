b3d7c7241e1f78ac237e180180d092da772e392b
User6 <User6@CBAPOOL-0>
2020-08-04 17:11:32 +0000

    Add new changes

---
+++ SampleAssocaiteCode/UserService/IUserService.cs
@@ -0,0 +1,16 @@
+﻿using SampleAssocaiteCode.EntityModel;
+using SampleAssocaiteCode.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SampleAssocaiteCode.UserService
+{
+    public interface IUserService
+    {
+        Task<UserRolesDTO> Authenticate(string username, string password);
+        Task<IQueryable<User>> GetAll();
+        Task<User> GetById(int id);
+    }
+}
