6ad1ae7d3a4c630556d5891d8c782127fc52f6b3
User6 <User6@CBAPOOL-0>
2020-08-04 16:58:52 +0000

    Commiting All Five Changes

---
+++ SampleAssocaiteCode/EntityModel/ResourceDetail.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SampleAssocaiteCode.EntityModel
+{
+    public class ResourceDetail
+    {
+        [Key]
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PrimarySkills { get; set; }
+        public string SecondarySkills { get; set; }
+        public string Location { get; set; }
+        public int Experience { get; set; }
+        public bool? IsRecommendedForTraining { get; set; }
+    }
+}
