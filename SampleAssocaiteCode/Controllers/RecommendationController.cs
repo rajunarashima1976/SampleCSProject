4b0d94d8ac400942c23b3e0debc0ed5cd9044a29
User6 <User6@CBAPOOL-0>
2020-08-04 18:17:58 +0000

    Add new chanages

---
+++ SampleAssocaiteCode/Controllers/RecommendationController.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SampleAssocaiteCode.EntityModel;
+using SampleAssocaiteCode.RecommendationService;
+
+namespace SampleAssocaiteCode.Controllers
+{
+    [Route("api/recommendation/")]
+    [ApiController]
+    public class RecommendationController : ControllerBase
+    {
+        private readonly IRecommendationService _recommendationService;
+        private readonly IMapper _mapper;
+
+        public RecommendationController(IRecommendationService recommendationService, IMapper mapper)
+        {
+            _recommendationService = recommendationService;
+            _mapper = mapper;
+        }
+        [HttpGet]
+        [Authorize(Roles = "BM")]
+        [Route("seek")]
+        public async Task<IActionResult> Seek()
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            var response = await _recommendationService.Seek();
+            return Ok(response);
+        }
+        [HttpPost("routerecommendation")]
+        [Authorize(Roles = "BM")]
+        public async Task<IActionResult> RouteToPM([FromBody] List<Recommendation> recommendation)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            var response = await _recommendationService.RouteToPM(recommendation);
+            return Ok(response);
+        }
+
+        [HttpGet]
+        [Authorize(Roles = "PM")]
+        public async Task<IActionResult> Get()
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+            var response = await _recommendationService.Get();
+            return Ok(response);
+        }
+        [HttpPost("accept")]
+        [Authorize(Roles = "PM")]
+        public async Task<IActionResult> Accept([FromBody] int Id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+            var response = await _recommendationService.Accept(Id);
+            return Ok(response);
+        }
+
+        [HttpPost("reject")]
+        [Authorize(Roles = "PM")]
+        public async Task<IActionResult> Reject([FromBody] int Id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+            var response = await _recommendationService.Reject(Id);
+            return Ok(response);
+        }
+    }
+}
---
+++ SampleAssocaiteCode/Controllers/UsersController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SampleAssocaiteCode.Models;
+using SampleAssocaiteCode.UserService;
+
+namespace SampleAssocaiteCode.Controllers
+{
+    [Authorize]
+    [Route("api/user")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IUserService _userService;
+        private readonly IMapper _mapper;
+
+        public UsersController(IUserService userService, IMapper mapper)
+        {
+            _userService = userService;
+            _mapper = mapper;
+        }
+
+        [AllowAnonymous]
+        [HttpPost("/token")]
+        public async Task<IActionResult> Authenticate([FromBody] UserDTO user)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+            var result = await _userService.Authenticate(user.Username, user.Password);
+
+            if (result == null)
+                return BadRequest(new { message = "Username or password is incorrect" });
+
+            return Ok(result);
+        }
+
+        [HttpGet]
+        [Authorize(Roles = "PM")]
+        public async Task<IActionResult> GetAll()
+        {
+            var users = await _userService.GetAll();
+            return Ok(users);
+        }
+
+        [HttpGet]
+        [Authorize(Roles = "PM")]
+        [Route("id")]
+        public async Task<IActionResult> Get(int Id)
+        {
+            var users = await _userService.GetById(Id);
+            return Ok(users);
+        }
+    }
+}
